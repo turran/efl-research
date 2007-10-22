1d0042020e1c76fcc9ce68dbe0da0efefa812e4d
Jorge Zapata <jorgeluis.zapata@gmail.com>
2007-10-22 16:35:53 +0000

    + Initial code layout

--- esvg/src/lib/esvg_private.h
+++ esvg/src/lib/esvg_private.h
@@ -6,4 +6,46 @@
 #include "Enesim.h"
 #include "EXML.h"
 
+enum
+{
+	ESVG_ELEMENT_GROUP,
+	ESVG_ELEMENT_POLYLINE,
+	ESVG_ELEMENT_POLYGON,
+	ESVG_ELEMENTS
+};
+
+enum
+{
+
+	ESVG_ATTRIBUTES
+};
+
+
+typedef void (*ESVG_Element_Parser)(void);
+typedef void (*ESVG_Attribute_Parser)(void);
+
+typedef struct _ESVG_Element
+{
+	const char *name;
+	ESVG_Element_Parser *parser;	
+} ESVG_Element;
+
+typedef struct _ESVG_Attribute
+{
+	const char *name;
+	ESVG_Attribute_Parser *parser;	
+} ESVG_Attribute;
+
+ESVG_Element esvg_elements[ESVG_ELEMENTS] = {
+	.[ESVG_ELEMENT_GROUP] = {
+		.name = "g",
+	},
+	.[ESVG_ELEMENT_POLYLINE] = {
+		.name = "polyline",
+	},
+	.[ESVG_ELEMENT_POLYGON] = {
+		.name = "polygon",
+	},
+};
+
 #endif
---
+++ esvg/src/lib/polygon.c
@@ -0,0 +1,11 @@
+#include "ESVG.h"
+#include "esvg_private.h"
+
+ESVG_Elements[ESVG_ELEMENT_POLYGON] = {
+
+};
+
+static void polygon_parse(void)
+{
+
+}
