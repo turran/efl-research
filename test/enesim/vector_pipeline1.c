021d3f104e35391e5b58408afb0ad913d6a40215
Jorge Zapata <jorgeluis.zapata@gmail.com>
2007-12-07 19:36:43 +0000

    + Update to current enesim API + add a star to test even/odd-zero rasterizer

--- test/enesim/vector_pipeline1.c
+++ test/enesim/vector_pipeline1.c
@@ -3,6 +3,9 @@
 #include <Enesim.h>
 
 #define DATA "../data"
+#define WIDTH 500
+#define HEIGHT 800
+
 
 struct Renderer
 {
@@ -10,19 +13,11 @@ struct Renderer
 	Enesim_Surface *s;
 };
 
-void kiia_scanline_callback(void *sl_data, void *data)
+void kiia_scanline_callback(void *sl_data, int sl_type, void *data)
 {
 	Enesim_Scanline_Mask *sl = sl_data;
 	struct Renderer *rend = data;
-	int i;
-	//printf("called %p y = %d x = %d w = %d!!!\n", rend, sl->y, sl->x, sl->w);
-	#if 0 
-	for (i = 0; i < sl->w; i++)
-	{
-		printf("%x\n", sl->coverages[i]);
-	}
-	#endif
-	enesim_renderer_draw(rend->r, ENESIM_SCANLINE_MASK, sl, rend->s);
+	enesim_renderer_draw(rend->r, sl_type, sl, rend->s);
 }
 
 static void kiia_rasterizer(Enesim_Component_Reader *r)
@@ -30,16 +25,17 @@ static void kiia_rasterizer(Enesim_Component_Reader *r)
 	float x, y;
 	int cmd;
 	struct Renderer rend;
+	struct timeval ts, te;
 
 	Enesim_Rasterizer *rs;
 	Enesim_Scanline *sl;
 	Enesim_Renderer *rd;
-	DATA32 *d32 = calloc(1, sizeof(DATA32) * 400 * 400);
+	DATA32 *d32 = calloc(1, sizeof(DATA32) * WIDTH * HEIGHT);
 	Enesim_Surface *dst;
 	Enesim_Rectangle rect;
 
-	enesim_rectangle_from_coords(&rect, 0, 0, 400, 400);
-	rs = enesim_rasterizer_kiia_new(ENESIM_RASTERIZER_KIIA_COUNT_8, rect);
+	enesim_rectangle_from_coords(&rect, 0, 0, WIDTH, HEIGHT);
+	rs = enesim_rasterizer_kiia_new(ENESIM_RASTERIZER_KIIA_COUNT_16, rect);
 	while ((cmd = enesim_reader_vertex_get(r, &x, &y)) != ENESIM_CMD_END)
 	{
 		enesim_rasterizer_vertex_add(rs, x, y);
@@ -47,21 +43,25 @@ static void kiia_rasterizer(Enesim_Component_Reader *r)
 	sl = enesim_scanline_alias_new();
 	rd = enesim_fill_color_new();
 	enesim_fill_color_color_set(rd, 0xff0000ff);
-	dst = enesim_surface_new(ENESIM_SURFACE_ARGB8888, 400, 400, ENESIM_SURFACE_ALPHA, d32);
+	dst = enesim_surface_new(ENESIM_SURFACE_ARGB8888, WIDTH, HEIGHT, ENESIM_SURFACE_ALPHA, d32);
 	rend.r = rd;
 	rend.s = dst;
-	enesim_rasterizer_generate(rs, ENESIM_SCANLINE_MASK, kiia_scanline_callback, &rend);
+	gettimeofday(&ts, NULL);
+	enesim_rasterizer_generate(rs, kiia_scanline_callback, &rend);
+	gettimeofday(&te, NULL);
+	printf("KIIA: \n");
+	time_display(ts, te);
+
 	png_save(dst, "/tmp/enesim_kiia.png", 0);
 	enesim_rasterizer_delete(rs);
 }
 
 
-void cpsc_scanline_callback(void *sl_data, void *data)
+void cpsc_scanline_callback(void *sl_data, int sl_type, void *data)
 {
 	Enesim_Scanline_Alias *sl = sl_data;
 	struct Renderer *rend = data;
-	//printf("called %p y = %d x = %d w = %d!!!\n", rend, sl->y, sl->x, sl->w);
-	enesim_renderer_draw(rend->r, ENESIM_SCANLINE_ALIAS, sl, rend->s);
+	enesim_renderer_draw(rend->r, sl_type, sl, rend->s);
 }
 
 static void cpsc_rasterizer(Enesim_Component_Reader *r)
@@ -69,26 +69,32 @@ static void cpsc_rasterizer(Enesim_Component_Reader *r)
 	float x, y;
 	int cmd;
 	struct Renderer rend;
+	struct timeval ts, te;
 
 	Enesim_Rasterizer *rs;
 	Enesim_Scanline *sl;
 	Enesim_Renderer *rd;
-	DATA32 *d32 = calloc(1, sizeof(DATA32) * 400 * 400);
+	DATA32 *d32 = calloc(1, sizeof(DATA32) * WIDTH * HEIGHT);
 	Enesim_Surface *dst;
 	Enesim_Rectangle rect;
 
-	enesim_rectangle_from_coords(&rect, 0, 0, 400, 400);
+	enesim_rectangle_from_coords(&rect, 0, 0, WIDTH, HEIGHT);
 	rs = enesim_rasterizer_cpsc_new(rect);
 	rd = enesim_fill_color_new();
 	enesim_fill_color_color_set(rd, 0xff0000ff);
-	dst = enesim_surface_new(ENESIM_SURFACE_ARGB8888, 400, 400, ENESIM_SURFACE_ALPHA, d32);
+	dst = enesim_surface_new(ENESIM_SURFACE_ARGB8888, WIDTH, HEIGHT, ENESIM_SURFACE_ALPHA, d32);
 	while ((cmd = enesim_reader_vertex_get(r, &x, &y)) != ENESIM_CMD_END)
 	{
 		enesim_rasterizer_vertex_add(rs, x, y);
 	}
 	rend.r = rd;
 	rend.s = dst;
-	enesim_rasterizer_generate(rs, ENESIM_SCANLINE_ALIAS, cpsc_scanline_callback, &rend);
+
+	gettimeofday(&ts, NULL);
+	enesim_rasterizer_generate(rs, cpsc_scanline_callback, &rend);
+	gettimeofday(&te, NULL);
+	printf("CPSC: \n");
+	time_display(ts, te);
 	png_save(dst, "/tmp/enesim_cpsc.png", 0);
 	enesim_rasterizer_delete(rs);
 }
