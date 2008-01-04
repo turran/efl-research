710028c6fabfc7e68f49e5d0938572f3ffe06cb2
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-01-04 13:18:16 +0000

    + Remove enesim from here, e cvs's enesim will be here soon :)

--- enesim/src/lib/raster/surface/rop/rgb565.c
+++
@@ -1,5 +0,0 @@
-#include "Enesim.h"
-#include "enesim_private.h"
-#include "surface.h"
-
-Enesim_Surface_Func rgb565_funcs[ENESIM_RENDERER_ROPS];
--- enesim/src/lib/vector/enesim_vector.h
+++
@@ -1,8 +0,0 @@
-#ifndef _VECTOR_H
-#define _VECTOR_H
-
-#include "container.h"
-#include "component.h"
-#include "reader.h"
-
-#endif
