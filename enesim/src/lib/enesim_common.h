00a2eb4a90757419c635290abf11da3a2730ba6c
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-01-17 11:54:58 +0000

    + Missed file

---
+++ enesim/src/lib/enesim_common.h
@@ -0,0 +1,13 @@
+#ifndef _ENESIM_COMMON_H
+#define _ENESIM_COMMON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <math.h>
+
+#include "Eina.h"
+
+#endif
+
