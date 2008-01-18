ae7eebb1dd078999025e06b112b1ab8f094b49b0
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-01-18 19:25:57 +0000

    stubs

--- esvg/src/lib/ESVG.h
+++ esvg/src/lib/ESVG.h
@@ -5,7 +5,25 @@
  * TODO split the code in two, the drawing and the parser =)
  */
 typedef struct _ESVG ESVG;
-EAPI void esvg_file_save(const char *file);
-EAPI ESVG* esvg_file_load(const char *file);
+typedef struct _ESVG_Shape ESVG_Shape;
+
+EAPI ESVG_Shape * esvg_shape_rectangle_add(ESVG *e);
+EAPI void esvg_shape_rectangle_geometry_set(ESVG_Shape *s, Enesim_Rectangle *r);
+EAPI void esvg_shape_rectangle_rounding_set(ESVG_Shape *s, float rx, float ry);
+
+EAPI ESVG_Shape * esvg_shape_ellipse_add(ESVG *e);
+EAPI ESVG_Shape * esvg_shape_polygon_add(ESVG *e);
+EAPI ESVG_Shape * esvg_shape_polyline_add(ESVG *e);
+EAPI ESVG_Shape * esvg_shape_circle_add(ESVG *e);
+EAPI ESVG_Shape * esvg_shape_line_add(ESVG *e);
+EAPI void esvg_shape_line_xy_set(ESVG_Shape *s, float x1, float y1, float x2, float y2);
+
+EAPI void esvg_shape_translate(ESVG_Shape *s, float x, float y);
+EAPI void esvg_shape_scale(ESVG_Shape *s, float w, float h);
+EAPI void esvg_shape_rotate(ESVG_Shape *s, float r);
+
+/* parsing */
+//EAPI void esvg_file_save(const char *file);
+//EAPI ESVG* esvg_file_load(const char *file);
 
 #endif
---
+++ esvg/src/lib/draw/shape.c
@@ -0,0 +1,8 @@
+#include "esvg_common.h"
+#include "ESVG.h"
+#include "esvg_private.h"
+
+struct _ESVG_Shape
+{
+	Ekeko_Object *object;
+};
