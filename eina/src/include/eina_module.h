2250d6ac8fcc241081bd010108c471fcb2501c05
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-03-31 22:20:25 +0000

    + Fix some warning

--- eina/src/include/eina_module.h
+++ eina/src/include/eina_module.h
@@ -6,7 +6,7 @@ typedef struct _Eina_Module Eina_Module;
 typedef void (*Eina_Module_Load_Cb)(Eina_Module *m, void *data);
 
 EAPI Eina_Module * eina_module_load(const char *name);
-EAPI eina_module_load_all(const char *dir, Eina_Module_Load_Cb cb, void *data);
+EAPI void eina_module_load_all(const char *dir, Eina_Module_Load_Cb cb, void *data);
 EAPI void eina_module_unload(Eina_Module *m);
 EAPI void * eina_module_symbol_get(Eina_Module *m, const char *symbol);
 
