aa12e17f6c0c23b1ce17c731f9fdcdbdb4b9f1a8
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-04-09 15:44:43 +0000

    + Private headers without the prefix + Add future source files

---
+++ enesim/src/lib/enesim_converter.c
@@ -0,0 +1,15 @@
+#include "enesim_common.h"
+#include "Enesim.h"
+#include "enesim_private.h"
+/*============================================================================*
+ *                                   API                                      * 
+ *============================================================================*/
+/**
+ * To be documented
+ * FIXME: To be fixed
+ */
+EAPI Enesim_Converter_Fnc enesim_converter_fnc_get(Enesim_Surface_Format stype,
+		Enesim_Surface_Format dtype)
+{
+	
+}
