5a2a9cbbd546cab753511aeb81a85114abaf6e21
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-04-20 17:19:48 +0000

    + Etch code in

--- test/etk-etch/main.c
+++ test/etk-etch/main.c
@@ -1,10 +1,90 @@
 #include <Etk.h>
+#include <Etch.h>
 
 Etk_Widget *window;
 Etk_Widget *canvas;
 Etk_Widget *etk_evas_object;
 Evas_Object *evas_object;
 Evas *evas;
+
+Etch *etch;
+Etch_Object *etch_object;
+
+
+int timer_cb(void *data)
+{
+	etch_timer_tick(etch);
+	return 1;
+}
+
+void _position_x_uint32(void *odata, void *pdata)
+{
+	int x, y;
+
+	x = *(unsigned int *)pdata;
+	printf("called with x %d\n", x);
+	evas_object_geometry_get(evas_object, NULL, &y, NULL, NULL);
+	evas_object_move(evas_object, x, y);
+}
+
+void _position_y_uint32(void *odata, void *pdata)
+{
+	int x, y;
+
+	y = *(unsigned int *)pdata;
+	printf("called with y %d\n", y);
+	evas_object_geometry_get(evas_object, &x, NULL, NULL, NULL);
+	evas_object_move(evas_object, x, y);
+}
+
+Etch_Object_Property oproperties[] = {
+	{
+		.type = ETCH_POSITION_X_UINT32,
+		.set = &_position_x_uint32, 
+	},
+	{
+		.type = ETCH_POSITION_Y_UINT32,
+		.set = &_position_y_uint32,
+	},
+};
+
+Etch_Object_Class oclass = {
+	.props = oproperties,
+};
+
+
+void etch_setup(void)
+{
+	Etch_Animation *ea;
+	Etch_Animation_Keyframe *ek;
+
+	etch = etch_new();
+	etch_timer_fps_set(etch, 30);
+	ecore_timer_add(1.0/30.0, timer_cb, NULL);
+	
+	etch_object = etch_object_add(etch, &oclass, "object1", NULL);
+
+	ea = etch_animation_new(ETCH_UINT32);
+	/* x coordinate */
+	ek = etch_animation_keyframe_add(ea);
+	etch_animation_keyframe_value_set(ek, ETCH_ANIMATION_QUADRATIC, 5, 5);
+	etch_animation_keyframe_time_set(ek, 0, 0);
+	
+	ek = etch_animation_keyframe_add(ea);
+	etch_animation_keyframe_value_set(ek, ETCH_ANIMATION_QUADRATIC, 10, 5);
+	etch_animation_keyframe_time_set(ek, 5, 0);
+	
+	ek = etch_animation_keyframe_add(ea);
+	etch_animation_keyframe_value_set(ek, ETCH_ANIMATION_QUADRATIC, 5, 5);
+	etch_animation_keyframe_time_set(ek, 10, 0);
+	
+	ek = etch_animation_keyframe_add(ea);
+	etch_animation_keyframe_value_set(ek, ETCH_ANIMATION_QUADRATIC, 0, 5);
+	etch_animation_keyframe_time_set(ek, 15, 0);
+	
+	/* append the animation to the object */
+	etch_object_animation_set(etch_object, ETCH_POSITION_X, ea);
+}
 
 int main(int argc, char **argv)
 {
@@ -27,6 +107,7 @@ int main(int argc, char **argv)
 
 	etk_widget_show_all(window);
 
+	etch_setup();
 	etk_main();
 	etk_shutdown();
 
