533f7a50a305c851cfab7ed46e5496b1fcd1dd73
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-04-28 20:43:06 +0000

    + Missing file

---
+++ esvg/src/lib/esvg_draw_private.h
@@ -0,0 +1,85 @@
+#ifndef ESVG_DRAW_PRIVATE_H_
+#define ESVG_DRAW_PRIVATE_H_
+
+/* id
+ * xml:base
+ * xml:lang
+ * xml:space
+ */
+typedef struct _ESVG_Attribute_Core
+{
+	
+} ESVG_Attribute_Core;
+
+
+/* color 
+ * fill
+ * fill-rule
+ * stroke
+ * stroke-dasharray
+ * stroke-dashoffset
+ * stroke-linecap
+ * stroke-linejoin
+ * stroke-miterlimit
+ * stroke-width
+ * color-interpolation
+ * color-rendering
+ */
+typedef struct _ESVG_Attribute_Paint
+{
+	
+} ESVG_Attribute_Paint;
+
+/* opacity
+ * stroke-opacity
+ * fill-opacity
+ */
+typedef struct _ESVG_Attribute_Opacity
+{
+	
+} ESVG_Attribute_Opacity;
+
+
+/* display
+ * image-rendering
+ * pointer-events
+ * shape-rendering
+ * text-rendering
+ * visibility
+ */
+typedef struct _ESVG_Attribute_Graphics
+{
+	
+} ESVG_Attribute_Graphics;
+
+/* clip-path
+ * clip-rule
+ */
+typedef struct _ESVG_Attribute_Clip
+{
+	
+} ESVG_Attribute_Clip;
+
+/* mask
+ */
+typedef struct _ESVG_Attribute_Mask
+{
+	
+} ESVG_Attribute_Mask;
+
+/*  Filter.attrib  	filter
+FilterColor.attrib 	color-interpolation-filters
+FilterPrimitive.attrib 	x, y, width, height, result
+FilterPrimitiveWithIn.attrib 	FilterPrimitive.attrib, in
+*/
+
+/* cursor */
+typedef struct _ESVG_Attribute_Cursor
+{
+	
+} ESVG_Attribute_Cursor;
+
+
+
+
+#endif /*ESVG_DRAW_PRIVATE_H_*/
