d25e7c68f5966f3ad2ef3f9d39cb830cbb67f9e7
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-04-28 22:57:07 +0000

    + Missing file

---
+++ esvg/src/lib/esvg_common.h
@@ -0,0 +1,11 @@
+#ifndef ESVG_COMMON_H_
+#define ESVG_COMMON_H_
+
+#include <stdlib.h>
+
+#include "Eina.h"
+#include "Enesim.h"
+#include "Ekeko.h"
+#include "openvg.h"
+
+#endif /*ESVG_COMMON_H_*/
