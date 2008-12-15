8d9194d79ae890f29f1b6b12274c4eb3a166540c
Jorge Zapata <jorgeluis.zapata@gmail.com>
2008-12-15 12:38:06 +0000

    More testing

--- eix/src/bin/main.c
+++ eix/src/bin/main.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 
 #include "Eix.h"
 #include "EXML.h"
@@ -12,16 +13,19 @@ typedef struct _Eix
 	Ecore_Evas *ee;
 	Evas *e;
 	Etch *etch;
+	Eina_Inlist *objects;
 } Eix;
 
 typedef struct _Eix_Object
 {
+	EINA_INLIST;
 	Evas_Object *o;
 	Enesim_Matrix *m;
 	Enesim_Surface *src;
 	Enesim_Surface *dst;
 	Eina_Bool render;
 	Enesim_Transformation *tx;
+	float angle;
 } Eix_Object;
 
 Eix_Object * eix_object_add(Eix *e);
@@ -30,9 +34,58 @@ Eix * eix_new(void);
 void eix_loop(void);
 Eina_Bool eix_file_load(Eix *e, const char *f);
 
+
+static inline void _transform_apply(Eix_Object *eo)
+{
+	Eina_Rectangle rect;
+	Enesim_Surface_Pixel color;
+	Enesim_Surface_Data sdata;
+	Enesim_Matrix *matrix;
+	int imgw, imgh;
+	
+	enesim_matrix_rotate(eo->m, eo->angle);
+	enesim_surface_data_get(eo->dst, &sdata);
+	enesim_surface_size_get(eo->dst, &rect.w, &rect.h);
+	enesim_surface_size_get(eo->dst, &imgw, &imgh);
+	//memset(sdata.data.argb8888.plane0, 0x0, imgw * imgh * 4);
+	eina_rectangle_coords_from(&rect, 0, 0, imgw, imgh);
+	//printf("%d %d\n", imgh/2, imgw/2);
+	//enesim_transformation_origin_set(o->tx, 0, 0);
+	enesim_transformation_origin_set(eo->tx, imgw/2, imgh/2);
+#if 1
+	matrix = enesim_matrix_new();
+	enesim_matrix_translate(matrix, -imgw/2, -imgh/2);
+	enesim_matrix_compose(eo->m, matrix, eo->m);
+	enesim_matrix_delete(matrix);
+#endif
+	enesim_transformation_matrix_set(eo->tx, eo->m);
+
+	enesim_surface_pixel_components_from(&color,
+		enesim_surface_format_get(eo->dst), 0xaa, 0xff, 0x45, 0x10);
+	enesim_transformation_color_set(eo->tx, &color);
+//	enesim_transformation_quality_set(eo->tx, ENESIM_FAST);
+	enesim_transformation_apply(eo->tx, eo->src, &rect, eo->dst, &rect);
+	evas_object_image_pixels_dirty_set(eo->o, 1);
+
+	eo->render = EINA_FALSE;
+	enesim_transformation_apply(eo->tx, eo->src, &rect, eo->dst, &rect);
+	evas_object_image_pixels_dirty_set(eo->o, 1);
+}
+
 void _pre_render(Ecore_Evas *ee)
 {
+	Eix *eix;
+	Eix_Object *eo;
+
+	eix = ecore_evas_data_get(ee, "eix");
 	/* iterate over the list of objects */
+	EINA_INLIST_FOREACH(eix->objects, eo)
+	{
+#if 0
+		if (eo->render)
+			_transform_apply(eo);
+#endif
+	}
 }
 
 static void _document_parse(EXML_Node *n)
@@ -56,77 +109,41 @@ void _childs_shape(Eix *e)
 }
 
 /* Properties callbacks */
-void _position_x_uint32(void *odata, void *pdata)
+void _position_x_uint32(const Etch_Data *curr, const Etch_Data *prev, void *data)
 {
-	Evas_Object *o = odata;
-	int x = *(unsigned int *)pdata;
+	Evas_Object *o = data;
+	int x = curr->data.u32;
 	int y;
-	
+
 	evas_object_geometry_get(o, NULL, &y, NULL, NULL);
 	//printf("moving to %d %d\n", x, y);
 	evas_object_move(o, x, y);
 }
 
-void _position_y_uint32(void *odata, void *pdata)
+void _position_y_uint32(const Etch_Data *curr, const Etch_Data *prev, void *data)
 {
-	Evas_Object *o = odata;
-	int y = *(unsigned int *)pdata;
+	Evas_Object *o = data;
+	int y = curr->data.u32;
 	int x;
-		
+
 	evas_object_geometry_get(o, &x, NULL, NULL, NULL);
 	evas_object_move(o, x, y);
 }
 
-void _transform_uint32(void *odata, void *pdata)
+void _transform_rotate_uint32(const Etch_Data *curr, const Etch_Data *prev, void *data)
 {
-	Eix_Object *o = odata;
-	Eina_Rectangle rect;
-	Enesim_Surface_Data data;
-	Enesim_Matrix *matrix;
-	int imgw, imgh;
-	int deg = *(unsigned int *)pdata;
-
-#if 1
-	enesim_matrix_rotate(o->m, (float)deg*(3.1416)/180);
-	enesim_surface_data_get(o->dst, &data);
-	enesim_surface_size_get(o->dst, &imgw, &imgh);
-	memset(data.data.argb8888.plane0, 0x0, imgw * imgh * 4);
-	eina_rectangle_coords_from(&rect, 0, 0, imgw, imgh);
-	//printf("%d %d\n", imgh/2, imgw/2);
-	//enesim_transformation_origin_set(o->tx, 0, 0);
-	enesim_transformation_origin_set(o->tx, imgw/2, imgh/2);
-#if 1
-	matrix = enesim_matrix_new();
-	enesim_matrix_translate(matrix, -imgw/2, -imgh/2);
-	enesim_matrix_compose(o->m, matrix, o->m);
-	enesim_matrix_delete(matrix);
-#endif
-	enesim_transformation_matrix_set(o->tx, o->m);
-	enesim_transformation_apply(o->tx, o->src, &rect, o->dst, &rect);
-	evas_object_image_pixels_dirty_set(o->o, 1);
+	Eix_Object *o = data;
+	int deg = curr->data.u32;
 	
-#endif
+	o->angle = (float)(deg*(3.1416)/180);
+	o->render = EINA_TRUE;
+	_transform_apply(o);
 }
 
-/* Simple object class to test all properties */
-Etch_Object_Property oproperties[] = {
-	{
-		.type = ETCH_POSITION_X_UINT32,
-		.set = &_position_x_uint32, 
-	},
-	{
-		.type = ETCH_POSITION_Y_UINT32,
-		.set = &_position_y_uint32,
-	},
-	{
-		.type = (ETCH_USER << 16) | (ETCH_UINT32),
-		.set = &_transform_uint32,
-	},
-};
-
-Etch_Object_Class oclass = {
-	.props = oproperties,
-};
+void _transform_scale_uint32(const Etch_Data *curr, const Etch_Data *prev, void *data)
+{
+	
+}
 
 void _time_parse(const char *s, int *sec, int *msec)
 {
@@ -135,7 +152,6 @@ void _time_parse(const char *s, int *sec, int *msec)
 	int total;
 	
 	total = strtol(s, &end, 10);
-	printf("total = %d\n", total);
 	*sec = total;
 	
 }
@@ -144,11 +160,10 @@ void _traverse_animate_transform(Eix *e, Eix_Object *o)
 {
 	EXML *xml = e->xml;
 	Etch_Animation *ea;
-	Etch_Object *eo;
 	Etch_Animation_Keyframe *ek;
 	char *a;
-	int prop;
 	int sec, msec;
+	int count = 1;
 	/* make the evas object use a double buffered object */
 	if (!o->src)
 	{
@@ -175,14 +190,19 @@ void _traverse_animate_transform(Eix *e, Eix_Object *o)
 	if (strcmp(a, "transform"))
 		return;
 	a = exml_attribute_get(xml, "type");
-	if (!strcmp(a, "rotate"))
+	if (strcmp(a, "rotate"))
 	{
-		prop = ETCH_USER;		
-	}
-	else
 		return;
-	ea = etch_animation_new(ETCH_UINT32);
-	eo = etch_object_add(e->etch, &oclass, "object2", o);
+	}
+	a = exml_attribute_get(xml, "repeatCount");
+	if (a)
+	{
+		if (!strcmp(a, "indefinity"))
+			count = 0;
+		else
+			count = atoi(a);
+	}
+	ea = etch_animation_add(e->etch, ETCH_UINT32, _transform_rotate_uint32, o);
 	ek = etch_animation_keyframe_add(ea);
 	etch_animation_keyframe_type_set(ek, ETCH_ANIMATION_LINEAR);
 	a = exml_attribute_get(xml, "from");
@@ -194,7 +214,7 @@ void _traverse_animate_transform(Eix *e, Eix_Object *o)
 	a = exml_attribute_get(xml, "dur");
 	_time_parse(a, &sec, &msec);
 	etch_animation_keyframe_time_set(ek, sec, msec);
-	etch_object_animation_set(eo, prop, ea);
+	etch_animation_repeat_set(ea, count);
 #endif
 }
 
@@ -202,26 +222,32 @@ void _traverse_animate(Eix *e, Evas_Object *o)
 {
 	EXML *xml = e->xml;
 	Etch_Animation *ea;
-	Etch_Object *eo;
 	Etch_Animation_Keyframe *ek;
 	char *a;
-	int prop;
+	Etch_Animation_Callback cb;
 	int sec, msec;
-	
+	int count = 1;
+
 	a = exml_attribute_get(xml, "attributeName");
 	if (!strcmp(a, "x"))
 	{
-		
-		prop = ETCH_POSITION_X;		
+		cb = _position_x_uint32;
 	}
 	else if (!strcmp(a, "y"))
 	{
-		prop = ETCH_POSITION_Y;
+		cb = _position_y_uint32;
 	}
 	else
 		return;
-	ea = etch_animation_new(ETCH_UINT32);
-	eo = etch_object_add(e->etch, &oclass, "object1", o);
+	a = exml_attribute_get(xml, "repeatCount");
+	if (a)
+	{
+		if (!strcmp(a, "indefinity"))
+			count = 0;
+		else
+			count = atoi(a);
+	}
+	ea = etch_animation_add(e->etch, ETCH_UINT32, cb, o);
 	ek = etch_animation_keyframe_add(ea);
 	etch_animation_keyframe_type_set(ek, ETCH_ANIMATION_LINEAR);
 	a = exml_attribute_get(xml, "from");
@@ -233,7 +259,7 @@ void _traverse_animate(Eix *e, Evas_Object *o)
 	a = exml_attribute_get(xml, "dur");
 	_time_parse(a, &sec, &msec);
 	etch_animation_keyframe_time_set(ek, sec, msec);
-	etch_object_animation_set(eo, prop, ea);
+	etch_animation_repeat_set(ea, count);
 }
 
 void _traverse_rect(Eix *e)
@@ -281,7 +307,6 @@ void _traverse_img(Eix *e)
 	char *a1, *a2;
 	int imgw, imgh;
 	
-	printf("ADDED\n");
 	eo = eix_object_add(e);
 	/* parse the attributes */
 	o = evas_object_image_add(e->e);
@@ -348,7 +373,7 @@ void _traverse_eix(Eix *e)
 	ecore_evas_show(e->ee);
 	/* the background object */
 	o = evas_object_rectangle_add(e->e);
-	evas_object_color_set(o, 255, 0, 0, 255);
+	evas_object_color_set(o, 255, 255, 255, 255);
 	evas_object_move(o, 0, 0);
 	evas_object_resize(o, atoi(w), atoi(h));
 	evas_object_show(o);
@@ -384,6 +409,8 @@ Eix_Object * eix_object_add(Eix *e)
 	Eix_Object *eo;
 	
 	eo = calloc(1, sizeof(Eix_Object));
+	e->objects = eina_inlist_append(e->objects, EINA_INLIST_GET(eo));
+
 	return eo;
 }
 
@@ -394,7 +421,7 @@ void eix_init(void)
 Eix * eix_new(void)
 {
 	Eix *e;
-	e = malloc(sizeof(Eix));
+	e = calloc(1, sizeof(Eix));
 	return e;
 }
 
