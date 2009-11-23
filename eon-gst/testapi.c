61e1c780c07adc6495835fcf3632625d8b71c8c4
Jorge Zapata <jorgeluis.zapata@gmail.com>
2009-11-23 18:34:41 +0000

    + Add more tests

--- eon-gst/testapi.c
+++ eon-gst/testapi.c
@@ -4,23 +4,40 @@
 
 Eon_Document *doc;
 Eon_Canvas *canvas;
-Eon_Buffer *b;
+
+static void paint_square_test(Eon_Paint_Square *ps)
+{
+	eon_paint_square_x_set(ps, 0);
+	eon_paint_square_y_set(ps, 0);
+	eon_paint_square_w_rel_set(ps, 100);
+	eon_paint_square_h_rel_set(ps, 100);
+}
+
+static void checker_test(void)
+{
+	Eon_Checker *ch;
+
+	ch = eon_checker_new(doc);
+	paint_square_test((Eon_Paint_Square *)ch);
+	ekeko_object_child_append(canvas, ch);
+}
 
 /* create the eon scene */
 static void scene_create(void)
 {
 	Eon_Rect *r;
 
-	doc = eon_document_new("sdl", 640, 480, "resizable");
-
+	doc = eon_document_new("sdl", 320, 240, "resizable");
 	canvas = eon_canvas_new(doc);
-	ekeko_object_child_append(doc, canvas);
 	printf("1\n");
-	eon_paint_square_x_rel_set(canvas, 0);
-	eon_paint_square_y_rel_set(canvas, 0);
+	eon_paint_square_x_set(canvas, 0);
+	eon_paint_square_y_set(canvas, 0);
 	eon_paint_square_w_rel_set(canvas, 100);
 	eon_paint_square_h_rel_set(canvas, 100);
-
+	ekeko_object_child_append(doc, canvas);
+	printf("2\n");
+	checker_test();
+#if 0
 	r = eon_rect_new(doc);
 	eon_rect_x_rel_set(r, 0);
 	eon_rect_y_rel_set(r, 0);
@@ -29,18 +46,6 @@ static void scene_create(void)
 	eon_shape_fill_color_set(r, 0xffffffff);
 	eon_shape_show(r);
 	ekeko_object_child_append(canvas, r);
-
-#if 0
-	r = eon_rect_new(doc);
-	ekeko_object_child_append(canvas, r);
-	eon_rect_x_rel_set(r, 25);
-	eon_rect_y_rel_set(r, 25);
-	eon_rect_w_rel_set(r, 50);
-	eon_rect_h_rel_set(r, 50);
-	eon_rect_corner_radius_set(r, 30);
-	eon_shape_stroke_width_set(r, 2);
-	eon_shape_stroke_color_set(r, 0xff000000);
-	eon_shape_show(r);
 #endif
 }
 
