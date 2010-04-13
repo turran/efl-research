0960dabb6c249f2930334f7bac221acc3d861cd7
Jorge Zapata <jorgeluis.zapata@gmail.com>
2010-04-13 17:37:21 +0000

    + Delete evas, we dont need/use it anymore + move the ecore/eina list to TEST

--- evas_engine_common/src/lib/compositor/blend/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MAIN_H
-#define __MAIN_H
-
-COMPOSITOR_PROTO_ALL(blend, argb8888, c)
-COMPOSITOR_PROTO_ALL(blend, rgb565_a5, c)
-
-#endif
--- evas_engine_common/src/lib/geometry.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GEOMETRY_H
-#define _GEOMETRY_H
-
-#include "geometry/scanline.h"
-
-#endif
